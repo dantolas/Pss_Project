f1210b04fe0734c4b5dfdc0872625bf92f28bc3d
“Samuel <kuta.samuel@gmail.com>
2023-01-25 22:46:32 +0100

    Progress

--- loadingScript.js
+++ loadingScript.js
@@ -1,3 +1,10 @@
+const divPondeli = document.querySelector("#pondeli");
+const divUtery = document.querySelector("#utery");
+const divStreda = document.querySelector("#streda");
+const divCtvrtek = document.querySelector("#ctvrtek");
+const divPatek = document.querySelector("#patek");
+
+
 let params = new URLSearchParams(window.location.search);
 var userdata = JSON.parse(params.get('data'));
 console.log(params.get('username'));
@@ -19,8 +26,8 @@ function AjaxPost() {
             success: function(res){  
                                     //do what you want here...
                                     
-                                    loadAjaxResponse(res);
-                                    
+                                    rozvrh = loadAjaxResponse(res);
+                                    createRozvrh(rozvrh);
                     },
             error: function() {
               alert('Not Okay');
@@ -33,21 +40,61 @@ function AjaxPost() {
 
 function loadAjaxResponse(res){
         res = JSON.parse(res);
-        let rozvrh = [];
-        let pondeli = [];
-        let utery = [];
-        let streda = [];
-        let ctvrtek = [];
-        let patek = [];
+        let rozvrh = {};
+        rozvrh.pondeli =[];
+        rozvrh.utery =[];
+        rozvrh.streda =[];
+        rozvrh.ctvrtek =[];
+        rozvrh.patek =[];
+
+        
         res.forEach(predmet => {
-                if(predmet['Den'].toUpperCase() == 'PONDELI') pondeli.push(predmet);
-                if(predmet['Den'].toUpperCase() == 'UTERY') utery.push(predmet);
-                if(predmet['Den'].toUpperCase() == 'STREDA') streda.push(predmet);
-                if(predmet['Den'].toUpperCase() == 'CTVRTEK') ctvrtek.push(predmet);
-                if(predmet['Den'].toUpperCase() == 'PATEK') patek.push(predmet);
+                if(predmet['Den'].toUpperCase() == 'PONDELI') rozvrh.pondeli.push(predmet);
+                if(predmet['Den'].toUpperCase() == 'UTERY') rozvrh.utery.push(predmet);
+                if(predmet['Den'].toUpperCase() == 'STREDA') rozvrh.streda.push(predmet);
+                if(predmet['Den'].toUpperCase() == 'CTVRTEK') rozvrh.ctvrtek.push(predmet);
+                if(predmet['Den'].toUpperCase() == 'PATEK') rozvrh.patek.push(predmet);
         });
-        rozvrh.push(pondeli,utery,streda,ctvrtek,patek);
-        console.log(rozvrh);
-        //rozvrh.forEach(den => console.log(den));
+        
+        
+        
+        
+        return rozvrh;
         
 }
+
+
+function createRozvrh(rozvrh){
+        console.log("Creating Rozvrh");
+        console.log(rozvrh);
+        for(const key in rozvrh){
+                console.log(`${key}: ${rozvrh[key]}`);
+        }
+
+        
+
+        rozvrh.forEach(den => 
+        {       
+                console.log(den);
+                console.log(den['something'])
+                var denDiv = document.createElement('div');
+                denDiv.className = 'container';
+                denDiv.id = den['den'];
+                denDiv.innerHTML = den['den'];
+                
+                den.forEach(
+                predmet => 
+                {
+                        var div = document.createElement('div');
+                        div.id = den['predmet'];
+                        div.innerHTML = 'xd';
+                        
+                        
+                        denDiv.appendChild(div);
+                })
+                document.querySelector('#rozvrh').appendChild(denDiv);
+        })
+
+console.log("Rozvrh created");
+}
+    
